e372477f33dd682e0381d897fdca3b56924cdc8b
Immortal <ak2061435@gmail.com>
2023-01-02 19:15:35 +0530

    added frontend

---
+++ Public/JS/script.js
@@ -0,0 +1,16 @@
+
+function show() {
+
+    document.getElementById('menu-bar').style.cssText = "left:0%!important;";
+    document.getElementById('click').className = "fa fa-times icon";
+    document.getElementById('click').setAttribute("onclick", "hide()");
+
+}
+
+function hide() {
+
+    document.getElementById('menu-bar').style.cssText = "left:-100%!important;";
+    document.getElementById('click').className = "fa fa-bars icon";
+    document.getElementById('click').setAttribute("onclick", "show()");
+
+}
