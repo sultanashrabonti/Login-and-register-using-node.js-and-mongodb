166cef73a659f88a1696df7c2ff79cef660f970a
Immortal <ak2061435@gmail.com>
2023-01-03 09:25:29 +0530

    basic validation added

--- Public/JS/script.js
+++ Public/JS/script.js
@@ -14,3 +14,24 @@ function hide() {
     document.getElementById('click').setAttribute("onclick", "show()");
 
 }
+
+const pass = document.getElementById('pass')
+const confirmPass = document.getElementById('confirm')
+
+const register = document.getElementById('register')
+const email = document.getElementById('email')
+register.addEventListener('click', checkingConfrimPassword)
+
+
+function checkingConfrimPassword() {
+
+
+    if (pass.value != confirmPass.value) {
+        alert('the password and confirm password are not same')
+    } else if (pass.value != '' && confirmPass != '' && pass.value === confirmPass.value) {
+        alert('signed in sucessfully now go to login page and log yourself in ')
+        module.exports = [email.value, pass.value, confirmPass.value]
+    } else {
+        alert('empty fields')
+    }
+}
